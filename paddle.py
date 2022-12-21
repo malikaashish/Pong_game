31a0732a124ae0955c57c94d50a6abd102dedc03
Aashish Malik <aashishmalik3105@gmail.com>
2022-12-21 23:47:38 +0530

    Initial commmit

---
+++ paddle.py
@@ -0,0 +1,48 @@
+from turtle import Turtle
+
+class Paddle(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.penup()
+        self.goto(position)
+
+    def go_up(self):
+        y = self.ycor() + 20
+        self.goto(self.xcor(), y)
+
+    def go_down(self):
+        y = self.ycor() - 20
+        self.goto(self.xcor(), y)
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.shapesize(stretch_len=1, stretch_wid=1)
+        self.penup()
+        self.x_move=10
+        self.y_move=10
+        self.move_speed=0.1
+
+    def move(self):
+        x= self.xcor() + self.x_move
+        y =self.ycor() + self.y_move
+        self.goto(x,y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0,0)
+        self.x_move *= -1
+        self.move_speed=0.1
